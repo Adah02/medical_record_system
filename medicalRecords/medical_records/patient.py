01bd0ae1dda5fa9d7bcf4cefac1eb39deacc2688
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-02 21:24:42 +0100

    Medical record system for hospitals and medical centres

--- medicalRecords/medical_records/patient.py
+++ medicalRecords/medical_records/patient.py
@@ -1,9 +1,11 @@
+import re
+import sys
 from medical_records.gender import Gender
 from medical_records.medical_history import MedicalHistory
 from datetime import datetime
 
 class Patient:
-    def __init__(self, user_name: str, pass_word: str, id: int, firstname: str, lastname: str, sex: str, dob: str, phone_number: str):
+    def __init__(self, user_name: str, pass_word: str, id: int, firstname: str, lastname: str, sex: str, dob: str, phone: str):
         self.username = user_name
         self.password = pass_word
         self.patient_id = self.patient_id_validation(id)
@@ -11,7 +13,7 @@ class Patient:
         self.last_name = self.validate_last_name(lastname)
         self.gender = self.validate_gender(sex)
         self.date_of_birth = dob
-        self.phone_number = phone_number
+        self.phone_number = self.validate_phone_number(phone)
         self.__medical_history = list()
 
     def create_medical_history(self, record_id: int, past_illness: str, present_illness: str, allergies: str, medications: str) -> None:
@@ -24,13 +26,13 @@ class Patient:
 
     @staticmethod
     def patient_id_validation(id_number):
-        if not id_number.isdigit() and id_number <= 0:
+        if  0 > id_number > sys.maxsize:
             raise ValueError("Patient ID number must be greater than zero.")
         return id_number
 
     @staticmethod
     def validate_first_name(firstname: str) -> str:
-        if not first_name.isalpha():
+        if not firstname.isalpha():
             raise ValueError("Firstname must be alphabets.")
         return firstname.title()
 
@@ -43,21 +45,18 @@ class Patient:
     @staticmethod
     def validate_gender(gender):
         if not gender.lower() in [x.value.lower() for x in Gender]:
-            return "gender must be MALE or FEMALE"
+            raise ValueError("Gender must Male or Female.")
         return gender.title()
 
     @staticmethod
-    def validate_phone_number(self, phone: str):
-        if not phone.isdigit() and 10 < len(phone) > 11:
+    def validate_phone_number(phone: str) -> str:
+        number_format = re.findall(r'(0)([7-9])(0)([0-9]+)', phone)
+        if not phone.isdigit() or 10 < len(phone) > 11:
             raise ValueError("Phone number must be between 10 and 11")
-        self.phone_number = phone
+        return phone
 
-    @staticmethod
-    def validate_patient_data(first_name, last_name, gender, dob, phone_number):
-        validate_full_name(full_name, last_name)
-        validate_date_of_birth(dob)
-        patients_gender(gender)
-        validate_phone_number(phone)
+    def get_medical_history(self):
+        return len(self.__medical_history)
 
     def __str__(self):
         return f'{self.patient_id}\n{self.first_name.title()} {self.last_name.title()}\n{self.date_of_birth}\n{self.phone_number}'
--- medicalRecords/test/medical_records/test_patient.py
+++ medicalRecords/test/medical_records/test_patient.py
@@ -1,6 +1,38 @@
 from unittest import TestCase
-
 from medical_records.patient import Patient
 
 class TestPatient(TestCase):
-    records =
+    patient = Patient('adah02', '1234', 1, 'Emma',
+                      'Adah', 'male', '17,04,1998', '08160509785')
+
+    def test_that_medical_history_is_empty(self):
+        self.assertEqual(0, self.patient.get_medical_history())
+
+    def test_that_medical_history_is_not_empty(self):
+        self.patient.create_medical_history(12, 'head-ache', 'Sneezing', 'ampecillin', 'chloramphenical')
+        self.assertEqual(1, self.patient.get_medical_history())
+
+    def test_that_first_name_is_valid(self):
+        self.patient.validate_first_name("Emma")
+        self.assertEqual('Emma', self.patient.validate_first_name("Emma"))
+
+    def test_that_first_name_is_not_valid(self):
+        self.assertRaises(ValueError, self.patient.validate_first_name, "Adah02")
+
+    def test_that_last_name_is_valid(self):
+        self.patient.validate_last_name("Adah")
+        self.assertEqual('Adah', self.patient.validate_last_name("Adah"))
+
+    def test_that_last_name_is_not_valid(self):
+        self.assertRaises(ValueError, self.patient.validate_last_name, "Emma02")
+
+    def test_that_gender_is_valid(self):
+        self.patient.validate_gender("male")
+        self.assertEqual('Male', self.patient.validate_gender("male"))
+
+    def test_that_gender_is_not_valid(self):
+        self.assertRaises(ValueError, self.patient.validate_gender, "ma")
+
+    def test_that_phone_number_is_valid(self):
+        print(self.patient.validate_phone_number("0123456789"))
+        self.assertEqual('0123456789', self.patient.validate_phone_number("0123456789"))
